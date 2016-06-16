90be35088e17fb6f365b5a7610ac7f740c14a7c4
ramkrishnsaini <rkrishnsaini@hotmail.com>
2016-06-17 00:05:11 +0530

    Add existing file

--- navigating/modules/Repos.js
+++ navigating/modules/Repos.js
@@ -1,17 +1,54 @@
 import React from 'react'
 import NavLink from './NavLink'
 
+
 export default React.createClass({
     getInitialState () {
     return {
-    number:this.props.params.numId
+    number:this.props.params.numId,
+    data:{info:[]},
+    author: '',
+    text: '',
+    id:this.props.params.numId
 };
 },
-    handleNumberChange(e){
-        this.setState({number: e.target.value});
-
-},
+    handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+      
+  },
+    componentDidMount: function() {
+        var state=this;
+        $.ajax({
+          //type:"get",
+          url: "../data.json",
+          dataType: 'json',
+          cache: false,
+          //contentType: "application/json",
+          success: function(data) {
+              
+              
+              console.log('data',data);
+              var filteredArry=[];
+	      $.each(data.info,function(index, item ) {
+		      console.log(item);
+		      if(item.id==state.state.id)
+  filteredArry.push(item);
+});
+        this.setState({data: filteredArry});
+              console.log('filteredArry',filteredArry);
+              this.setState({author: filteredArry[0].author, text: filteredArry[0].text});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+      },
   render() {
+      var data = this.state.data.info; 
+      
     return (
       <div>
         <h2>Repos</h2>
@@ -21,12 +58,22 @@ export default React.createClass({
         </ul>
 <span>{this.props.params.numId}</span>
           {this.props.children}
-
-          <form>
-                                           author<input type="text" value={this.state.number}  onChange={this.handleNumberChange}/><br/>
-                                           text<input type="text"/><br/>
-                                           <input type="submit" value="submit"/>
-                                           </form>
+        
+  <form >
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
       </div>
     )
   }
